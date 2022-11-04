d988395e7c629704ec3b35f1f25d01c98c8e7518
nidhikumari <kumari9936nidhi@gmail.com>
2022-11-04 21:07:00 +0530

    mapped through the data file

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 import Layout from "../components/Layout";
+import ProductItems from "../components/ProductItems";
+import data from "../utils/data";
 
 export default function Home() {
-  return <Layout title="Home Page">Home Page</Layout>;
+  return (
+    <Layout title="Home Page">
+      <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
+        {data.products.map((product) => (
+          <ProductItems
+            product={product}
+            key={product.slug}
+          ></ProductItems>
+        ))}
+      </div>
+    </Layout>
+  );
 }
