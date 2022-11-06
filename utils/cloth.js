aea2d11743c23140350ac4a040ebf7af9eef570f
nidhikumari <kumari9936nidhi@gmail.com>
2022-11-06 16:40:25 +0530

    modified files

---
+++ utils/cloth.js
@@ -0,0 +1,13 @@
+const clothData = {
+  items: [
+    { variety: "clothing", name: "Clothing", image: "./images/cloth.jpg" },
+    { variety: "shoes", name: "Shoes", image: "./images/shoe.jpg" },
+    {
+      variety: "accessories",
+      name: "Accessories",
+      image: "/images/accesories.jpg",
+    },
+  ],
+};
+
+export default clothData;
