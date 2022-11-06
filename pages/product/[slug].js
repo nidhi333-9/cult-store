5f22a5d8985a94cd32dac0ae084a33c3677714cd
nidhikumari <kumari9936nidhi@gmail.com>
2022-11-06 21:25:41 +0530

    product details

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -4,6 +4,7 @@ import React from "react";
 import Layout from "../../components/Layout";
 import data from "../../utils/data";
 import { AiOutlineArrowLeft } from "react-icons/ai";
+import Image from "next/image";
 
 const ProductScreen = () => {
   const { query } = useRouter();
@@ -23,14 +24,41 @@ const ProductScreen = () => {
           Back to Product
         </Link>
       </div>
-      <h1>{product.name}</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-        consectetur deleniti, nemo laborum vitae doloremque inventore aspernatur
-        vel aut et iusto perferendis illum sit deserunt, blanditiis sed.
-        Perferendis, ducimus? Quo optio accusamus iusto! Placeat tempore fugit
-        numquam cum sapiente?
-      </p>
+      <div className="grid md:grid-cols-4 md:gap-3">
+        <div className="md:col-span-2">
+          <Image
+            src={product.image}
+            alt={product.name}
+            width={500}
+            height={500}
+            layout="responsive"
+          ></Image>
+        </div>
+        <div>
+          <ul>
+            <li>
+              <h1 className="text-lg">{product.name}</h1>
+            </li>
+            <li>Category: {product.category}</li>
+            <li>Brand : {product.brand}</li>
+            <li>
+              {product.rating} of {product.numReviews} reviews
+            </li>
+            <li>Description : {product.description}</li>
+          </ul>
+        </div>
+        <div className="card p-5">
+          <div className="mb-2 flex justify-between">
+            <div>Price</div>
+            <div>$ {product.price}</div>
+          </div>
+          <div className="mb-2 flex justify-between">
+            <div>Status</div>
+            <div>{product.countInStock > 0 ? "In stock" : "Unavailable"}</div>
+          </div>
+          <button className="primary-button w-full">Add to Cart</button>
+        </div>
+      </div>
     </Layout>
   );
 };
