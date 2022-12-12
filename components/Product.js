60f8c1b5e0185bc829b99bbc7c2f4f3fe2e5f525
nidhikumari <kumari9936nidhi@gmail.com>
2022-12-12 17:42:48 +0530

    initial changes

--- components/Product.js
+++ components/Product.js
@@ -6,7 +6,10 @@ import { useState } from "react";
 function Product({ id, title, price, description, category, image }) {
   const [rating] = useState(Math.floor(Math.random() * 5) + 1);
   return (
-    <div className=" card relative flex flex-col border m-5 z-30 p-10 bg-white">
+    <div
+      className=" card relative flex flex-col border m-5 z-30 p-10 bg-white"
+      key={id}
+    >
       <p className="absolute top-2 right-2 text-xs italic text-gray-400">
         {category}
       </p>
