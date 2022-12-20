25d6ad8cca7659dd8eff3b3e85c57363a5064bcf
nidhikumari <kumari9936nidhi@gmail.com>
2022-12-20 09:23:52 +0530

    working on responsiveness

--- components/ProductFeed.js
+++ components/ProductFeed.js
@@ -3,7 +3,7 @@ import Product from "./Product";
 
 function ProductFeed({ products }) {
   return (
-    <div className="grid grid-flow-row-dense mx-auto md:grid-cols-2 md:-mt-52 lg:grid-cols-3 xl:grid-cols-4 md:mx-5">
+    <div className="grid grid-flow-row-dense  md:grid-cols-2 md:-mt-52 lg:grid-cols-3 xl:grid-cols-4 md:mx-5 mx-auto">
       {products
         .slice(0, 4)
         .map(({ id, title, price, description, category, image }) => (
