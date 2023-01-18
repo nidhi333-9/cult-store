e789481b1bbce3197940cf2ab38560ab3f32dac6
nidhikumari <kumari9936nidhi@gmail.com>
2023-01-18 08:51:35 +0530

    final commit

--- pages/checkout.js
+++ pages/checkout.js
@@ -7,7 +7,7 @@ import CheckoutProduct from "../components/CheckoutProduct";
 import { loadStripe } from "@stripe/stripe-js";
 import axios from "axios";
 
-const stripePromise = loadStripe(process.env.stripe_public_key);
+const stripePromise = loadStripe(`${process.env.stripe_public_key}`);
 
 function Checkout() {
   const items = useSelector(selectItems);
